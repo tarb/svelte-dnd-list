696b202e1f25cb5de7843c862eb12bd0f47e966d
tarb <rtarbath@gmail.com>
2022-05-25 02:29:17 +1000

    updated sveltekit prerender to support gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,23 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		prerender: {
-			default: true,
-		},
 		adapter: adapter({
 			pages: 'docs',
-			assets: 'docs',
-		})
+		}),
+		paths: {
+			base: dev ? '' : '/svelte-dnd-list',
+		},
+		prerender: {
+			crawl: false,
+			default: true,
+		},
 	}
 };
 
