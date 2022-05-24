37c3d8e7a092b4da2c5faf41afc0b76a617b090d
tarb <rtarbath@gmail.com>
2022-05-25 02:06:25 +1000

    Update example build directory, build examples for gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		prerender: {
+			default: true,
+		},
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+		})
 	}
 };
 
